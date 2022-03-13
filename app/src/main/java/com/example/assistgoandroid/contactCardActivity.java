9fdcd9f3c05ad2da78516b9013a8c89130e795bb
Luna Dagci <ldagci@albany.edu>
2022-03-13 17:18:52 -0400

    Checkpoint 1

---
+++ app/src/main/java/com/example/assistgoandroid/contactCardActivity.java
@@ -0,0 +1,12 @@
+package com.example.assistgoandroid;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class contactCardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_card_activity);
+    }
+}
