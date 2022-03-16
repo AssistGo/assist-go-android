778e1cebe5e96ac6b4046ec2374b6129be452c28
Luna Dagci <ldagci@albany.edu>
2022-03-16 18:05:45 -0400

    Contact card completed

--- app/src/main/java/com/example/assistgoandroid/Contact/contactCardActivity.java
+++ app/src/main/java/com/example/assistgoandroid/Contact/contactCardActivity.java
@@ -1,30 +1,47 @@
 package com.example.assistgoandroid.Contact;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.assistgoandroid.R;
 import com.example.assistgoandroid.callActivity;
 import com.example.assistgoandroid.messageActivity;
 
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
 public class contactCardActivity extends AppCompatActivity {
+    String TAG = "ContactCard";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact_card_activity);
 
-//        Glide.with(context)
-//                .load(contact.getContactPicture())
-//                .override(400, 400)
-//                .centerCrop()
-//                .fitCenter() // scale to fit entire image within ImageView
-//                .transform(new RoundedCornersTransformation(200,10))
-//                .placeholder(R.drawable.loading_contact)
-//                .error(R.drawable.loading_contact)
-//                .into(holder.contactProfilePicture);
+        ImageView contactProfilePicture = findViewById(R.id.ivContactProfilePicture);
+        TextView contactName = findViewById(R.id.tvContactName);
+        TextView contactPhoneNumber = findViewById(R.id.tvPhoneNumber);
+
+        Contact contact = (Contact) getIntent().getParcelableExtra("CONTACT_CARD");
+        Log.i(TAG, "Contact is " + contact);
+
+        contactName.setText(contact.getName());
+        contactPhoneNumber.setText(contact.getPhoneNumber());
+        Glide.with(this)
+                .load(contact.getContactPicture())
+                .centerCrop()
+                .fitCenter() // scale to fit entire image within ImageView
+                .transform(new RoundedCornersTransformation(500,10))
+                    .placeholder(R.drawable.loading_contact)
+                    .error(R.drawable.loading_contact)
+                .into(contactProfilePicture);
+
     }
 
     public void onCallClick(View view){
