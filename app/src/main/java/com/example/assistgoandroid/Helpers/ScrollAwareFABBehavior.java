adaa664613c51c94d7e13082a06959a5dcbb4f7f
Luna Dagci <ldagci@albany.edu>
2022-03-28 20:29:33 -0400

    floating action button

---
+++ app/src/main/java/com/example/assistgoandroid/Helpers/ScrollAwareFABBehavior.java
@@ -0,0 +1,35 @@
+package com.example.assistgoandroid.Helpers;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
+
+    public ScrollAwareFABBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
+                               View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
+                dyUnconsumed, type);
+
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                @Override
+                public void onHidden(FloatingActionButton floatingActionButon) {
+                    super.onHidden(floatingActionButon);
+                    floatingActionButon.setVisibility(View.INVISIBLE);
+                }
+            });
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            child.show();
+        }
+    }
+}
