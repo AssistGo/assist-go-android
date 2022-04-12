135a35676e34b0546dcfb5015e65f335ddb3ebd5
jeongjieon <45721682+jeongjieon@users.noreply.github.com>
2022-04-12 13:12:52 -0400

    Finished Frontend 3 activity UI

---
+++ app/src/main/java/com/example/assistgoandroid/emergency/ambulanceActivity.java
@@ -0,0 +1,49 @@
+package com.example.assistgoandroid.emergency;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.assistgoandroid.R;
+
+public class ambulanceActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_CODE = 101;
+    Button editProfilePicture,changeNumber,editName;
+    ImageView userProfilePicture;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ambulance_page);
+    }
+
+        ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+                        if (data != null)
+                            userProfilePicture.setImageURI(data.getData());
+                    }
+                });
+                }
+
+
+
---
+++ app/src/main/java/com/example/assistgoandroid/emergency/policeActivity.java
@@ -0,0 +1,36 @@
+package com.example.assistgoandroid.emergency;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.assistgoandroid.R;
+
+public class policeActivity extends AppCompatActivity {
+    private static final int PERMISSION_CODE = 101;
+    Button editProfilePicture,changeNumber,editName;
+    ImageView userProfilePicture;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.police_page);
+    }
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    // There are no request codes
+                    Intent data = result.getData();
+                    if (data != null)
+                        userProfilePicture.setImageURI(data.getData());
+                }
+            });
+}
