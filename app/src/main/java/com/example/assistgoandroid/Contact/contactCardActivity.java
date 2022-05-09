faad04880c3370fc1a6698828a36055e06f2712d
Luna Dagci <ldagci@albany.edu>
2022-05-09 00:23:18 -0400

    Fix Call button

--- app/src/main/java/com/example/assistgoandroid/Contact/contactCardActivity.java
+++ app/src/main/java/com/example/assistgoandroid/Contact/contactCardActivity.java
@@ -10,6 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.example.assistgoandroid.Call.VideoCall;
 import com.example.assistgoandroid.Call.VideoCallRinging;
 import com.example.assistgoandroid.R;
 import com.example.assistgoandroid.emergency.DialerActivityGit;
@@ -56,13 +57,10 @@ public class contactCardActivity extends AppCompatActivity {
 
     public void onVideoCallClick(View view){
         //Intent intent = new Intent(this, VideoCallRinging.class);
-        /*
         Intent intent = new Intent(this, VideoCall.class);
 
         intent.putExtra(CONTACT_CARD, contact);
         this.startActivity(intent);
-
-         */
     }
 
     public void onMessageClick(View view){
