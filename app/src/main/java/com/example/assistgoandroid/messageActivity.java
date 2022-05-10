08465faefbc30edb36ce52f3243b795c34cf3067
Luna Dagci <ldagci@albany.edu>
2022-05-09 21:36:38 -0400

    Update gitignore and fix import

--- app/src/main/java/com/example/assistgoandroid/messageActivity.java
+++ app/src/main/java/com/example/assistgoandroid/messageActivity.java
@@ -3,6 +3,7 @@ package com.example.assistgoandroid;
 import android.Manifest;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
